41950e597e4f90c4ecdbbc3fce2d5d4e8bcc4d2a
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-11-26 10:25:23 +0100

    Making changes to server creation

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const http = require('http');
 
-let server = http.createServer((request, response) => {
-  let body = "What's up, man?";
-  response.writeHead(200, {
-    'Content-Length': body.length,
-    'Content-Type': 'text/plain'
-  });
-  response.end(body);
-})
-
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+http.createServer((req, res) =>{
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World\n');
+}).listen(3000);
