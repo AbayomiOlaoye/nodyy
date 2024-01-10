2dff5c0f006af2cb6a29c833210eca969edc9d82
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2024-01-10 15:56:11 +0100

    Edit server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((req, res) =>{
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(data);
+  res.end();
 }).listen(3000);
